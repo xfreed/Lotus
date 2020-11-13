a86310720506e1582a21092d89b67495b044dbbf
Michael Kunynets <xfreed1@gmail.com>
2020-11-13 17:19:54 +0000

    Add project files.

---
+++ Lotus.BBL/Start.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Lotus.BBL
+{
+    internal class Start
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
